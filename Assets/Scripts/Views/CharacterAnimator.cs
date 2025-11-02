10846586019bef1a323a8b53a94664638c0ad0aa
Gabriel Omar Guerrero <gaguerrero@uade.edu.ar>
2025-11-02 15:29:58 -0300

    feat: moved CharacterAnimator off the old Registry<T>

--- Assets/Scripts/Views/CharacterAnimator.cs
+++ Assets/Scripts/Views/CharacterAnimator.cs
@@ -3,25 +3,29 @@ using System.Collections.Generic;
 using DebugConsole.Commands;
 using UnityEngine;
 
-public class CharacterAnimator : Registry<CharacterAnimator>
+public class CharacterAnimator : MonoBehaviour
 {
-    [SerializeField] private Character character;
-    [SerializeField] private Animator animator;
-    [SerializeField] private SpriteRenderer spriteRenderer;
-    [SerializeField] private string speedParameter = "Speed";
-    [SerializeField] private string isJumpingParameter = "IsJumping";
-    [SerializeField] private string isFallingParameter = "IsFalling";
+    static readonly List<CharacterAnimator> instances = new List<CharacterAnimator>();
+    public static IReadOnlyList<CharacterAnimator> Instances => instances;
 
-    private bool _isOverriding = false;
-    private Coroutine _forceAnimationCoroutine;
-    private void Reset()
+    [SerializeField] Character character;
+    [SerializeField] Animator animator;
+    [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] string speedParameter = "Speed";
+    [SerializeField] string isJumpingParameter = "IsJumping";
+    [SerializeField] string isFallingParameter = "IsFalling";
+
+    bool isOverriding;
+    Coroutine forceAnimationCoroutine;
+
+    void Reset()
     {
         character = GetComponentInParent<Character>();
         animator = GetComponentInParent<Animator>();
         spriteRenderer = GetComponentInParent<SpriteRenderer>();
     }
 
-    private void Awake()
+    void Awake()
     {
         if (!character)
             character = GetComponentInParent<Character>();
@@ -31,9 +35,10 @@ public class CharacterAnimator : Registry<CharacterAnimator>
             spriteRenderer = GetComponentInParent<SpriteRenderer>();
     }
 
-    protected override void OnEnable()
+    void OnEnable()
     {
-        base.OnEnable();
+        if (!instances.Contains(this))
+            instances.Add(this);
         if (!character || !animator || !spriteRenderer)
         {
             Debug.LogError($"{name} <color=grey>({GetType().Name})</color>: At least one reference is null!");
@@ -41,9 +46,9 @@ public class CharacterAnimator : Registry<CharacterAnimator>
         }
     }
 
-    private void Update()
+    void Update()
     {
-        if (_isOverriding)
+        if (isOverriding)
             return;
         var speed = character.Velocity;
         animator.SetFloat(speedParameter, Mathf.Abs(speed.x));
@@ -51,21 +56,21 @@ public class CharacterAnimator : Registry<CharacterAnimator>
         animator.SetBool(isFallingParameter, character.Velocity.y < 0);
         spriteRenderer.flipX = speed.x < 0;
     }
-    
-    protected override void OnDisable()
+
+    void OnDisable()
     {
-        base.OnDisable(); 
+        instances.Remove(this);
     }
-    
+
     public void ForceAnimation(AnimationCommandConfig animConfig, float duration = 1f)
     {
-        if (_forceAnimationCoroutine != null)
-            StopCoroutine(_forceAnimationCoroutine);
-        
-        _forceAnimationCoroutine = StartCoroutine(ForceAnimationCoroutine(animConfig, duration));
+        if (forceAnimationCoroutine != null)
+            StopCoroutine(forceAnimationCoroutine);
+
+        forceAnimationCoroutine = StartCoroutine(ForceAnimationCoroutine(animConfig, duration));
     }
-    
-    private void ApplyParameter(AnimationCommandConfig.AnimationParameter param)
+
+    void ApplyParameter(AnimationCommandConfig.AnimationParameter param)
     {
         switch (param.type)
         {
@@ -81,9 +86,9 @@ public class CharacterAnimator : Registry<CharacterAnimator>
         }
     }
 
-    private IEnumerator ForceAnimationCoroutine(AnimationCommandConfig animConfig, float duration)
+    IEnumerator ForceAnimationCoroutine(AnimationCommandConfig animConfig, float duration)
     {
-        _isOverriding = true;
+        isOverriding = true;
 
         foreach (var param in animConfig.parameters)
             ApplyParameter(param);
@@ -95,7 +100,7 @@ public class CharacterAnimator : Registry<CharacterAnimator>
         foreach (var param in animConfig.exitParameters)
             ApplyParameter(param);
 
-        _isOverriding = false;
-        _forceAnimationCoroutine = null;
+        isOverriding = false;
+        forceAnimationCoroutine = null;
     }
 }
